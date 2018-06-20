6bb4503b770e9d20814f2d6e13b4966f010de72a
Ryan O'Flaherty <ryan.oflaherty@tallan.com>
2018-06-19 20:01:43 -0400

    add word add in functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+declare let Word;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor() { }
+
+  clearDocument() {
+    Word.run(function (context) {
+      const body = context.document.body;
+      body.clear();
+      return context.sync();
+    });
+    // .catch(errorHandler);
+  }
+
+  insertText() {
+    Word.run(function (context) {
+      const docBody = context.document.body;
+      docBody.insertParagraph('Lorem ipsum', Word.InsertLocation.end);
+      return context.sync();
+    });
+    // .catch(errorHandler);
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare let Office;
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+function launch() {
+  const platform = platformBrowserDynamic();
+  platform.bootstrapModule(AppModule);
+}
+
+if (window.hasOwnProperty('Office') && window.hasOwnProperty('Word')) {
+  // Application-specific initialization code goes into a function that is
+  // assigned to the Office.initialize event and runs after the Office.js initializes.
+  // Bootstrapping of the AppModule must come AFTER Office has initialized.
+  Office.initialize = reason => {
+    launch();
+  };
+} else {
+  launch();
+}
